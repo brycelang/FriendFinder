71442064157c8e03cb92f6f945b17d7b3180c2ed
bryce <brycelang@me.com>
2018-08-27 16:15:21 -0700

    test

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,17 @@
 var path = require("path");
 
 //exports the code so that we can grab the functions later on
-module.exports = function(app) {
+module.exports = function(init) {
 
 //(...).com/survey routes to the public/survery.html directory on the server and loads that html for the user.
-  app.get("/survey", function(req, res) {
+  init.get("/survey", function(req, res) {
+    console.log("dd")
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
   //  * wildcard incase we dont know where to route the user we send them back to the home page
-  app.get("*", function(req, res) {
+  init.get("*", function(req, res) {
+    console.log("gg")
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 
--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 var express = require("express");
 var bodyParse = require("body-parser");
 var signale = require("signale");
+var path = require("path");
 
 
 //basic express server being started on port 8080
 var init = express();
-var port =  8080;
+var port =  process.env.PORT || 880;
 
-init.use(bodyParser.urlencoded({ extended: true }));
+init.use(bodyParse.urlencoded({ extended: true }));
 // parse application/json
-init.use(bodyParser.json());
+init.use(bodyParse.json());
 
-require("./app/routing/apiRoutes")(init);
 require("./app/routing/htmlRoutes")(init);
 
 //instruct express to bind and listen to port 8080
 init.listen(port, function() {
-signale.success("Express server binded to port:8080");
+signale.success("Express server binded to port:" + port);
 });
